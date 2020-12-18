e56e3743dd71c9019ac1536bd970ba66b4dd6842
Yijian Huang <yijian.huang@duke.edu>
2020-12-18 13:37:12 -0500

    firebase podfile upload

--- Keep Up/Keep Up/AppDelegate.swift
+++ Keep Up/Keep Up/AppDelegate.swift
@@ -6,15 +6,18 @@
 // hi test
 
 import UIKit
+import Firebase
 
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
         return true
     }
 
---
+++ Keep Up/Pods/Target Support Files/Pods-Keep Up-Keep UpUITests/Pods-Keep Up-Keep UpUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Keep_Up_Keep_UpUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Keep_Up_Keep_UpUITests
+@end
