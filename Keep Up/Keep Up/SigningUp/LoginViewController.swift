e711c5951a8985d8eeb70dfbb1194a2b07eb7e59
Geshna Aggarwal <ga95@duke.edu>
2020-12-18 18:37:41 -0500

    login works!

---
+++ Keep Up/Keep Up/SigningUp/LoginViewController.swift
@@ -0,0 +1,103 @@
+//
+//  LoginViewController.swift
+//  Keep Up
+//
+//  Created by Geshna Aggarwal on 12/18/20.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setUpElements()
+    }
+    
+    func setUpElements() {
+        
+        //hide the error label
+        errorLabel.alpha = 0
+        
+        //style the elements
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(loginButton)
+        
+    }
+
+    //Check the fields and validate that the data is correct. If everything is correct, this
+    //method returns nil. Otherwise, it returns the error message
+    func validateFields() -> String? {
+        
+        //check that all fields are filled in
+        if emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields."
+        }
+        
+        return nil
+    }
+    
+
+    @IBAction func loginTapped(_ sender: Any) {
+        //validate text fields
+        
+        let error = validateFields()
+        
+        if error != nil {
+            //there's something wrong with the fields, show error message
+            showError(error!)
+            
+        }else{
+            
+            //create cleaned versions of the text field
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            //gets email from login
+            UserDefaults.standard.set(email, forKey: "keyEmail")
+            
+            //signing in the user
+            Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                
+                if error != nil {
+                    //couldn't sign in
+                    self.errorLabel.text = error!.localizedDescription
+                    self.errorLabel.alpha = 1
+                }else{
+                    //transition to home screen
+                    UserDefaults.standard.set(false, forKey: "check_name_2")
+                    let initialViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.initialViewController) as? InitialViewController
+                    
+                    self.view.window?.rootViewController = initialViewController
+                    self.view.window?.makeKeyAndVisible()
+                    
+                }
+                
+            }
+            
+        }
+    }
+    
+    func showError(_ message:String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    
+
+}
