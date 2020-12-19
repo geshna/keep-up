bdb0492670a78dc5d3393c7ad703baf3e8469f7b
Geshna Aggarwal <ga95@duke.edu>
2020-12-18 21:14:52 -0500

    not done yet just some initial changes

---
+++ Keep Up/Keep Up/Controllers/AddFriendViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddFriendViewController.swift
+//  Keep Up
+//
+//  Created by Geshna Aggarwal on 12/18/20.
+//
+
+import UIKit
+
+class AddFriendViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var lookButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func lookButtonPressed(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
