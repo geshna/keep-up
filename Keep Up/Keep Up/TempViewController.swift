00238bb735238b6069fe7cfaeb632d4aef8ca33f
Geshna Aggarwal <ga95@duke.edu>
2020-12-19 18:45:09 -0500

    temp VC

--- Keep Up/Keep Up/TempViewController.swift
+++ Keep Up/Keep Up/TempViewController.swift
@@ -13,16 +13,36 @@ import FirebaseFirestore
 class TempViewController: UIViewController {
 
     var dict: [String: Any] = ["name": "hi", "age": 7]
+    let db = Firestore.firestore()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let db = Firestore.firestore()
-        
-        db.collection("newcollection").document("haley@gmail.com").collection("myHabits").document("xhabit").collection("xday").addDocument(data:dict)
+                
+        //db.collection("newcollection").document("haley@gmail.com").collection("myHabits").document("xhabit").collection("xday").addDocument(data:dict)
+        addHabits()
     }
     
+    func addHabits() {
+        if Auth.auth().currentUser != nil {
+            let user = Auth.auth().currentUser
+            let userEmail = user?.email
+            var i = 0
+            
+            self.db.collection("users").document(userEmail!).collection("habits").document("xhabit").setData(dict)
+           
+            
+            
+            
+        } else {
+          // No user is signed in.
+          // ...
+            print("ERROR IN FINDING USER: NO USER IS SIGNED IN")
+        }
+        
+    }
 
     /*
     // MARK: - Navigation
