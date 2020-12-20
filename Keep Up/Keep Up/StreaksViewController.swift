26ccd04816a6ccd3b3b74e0f850594fc401e1a72
Lisa Tang <lt188@duke.edu>
2020-12-19 17:26:35 -0800

    init

---
+++ Keep Up/Keep Up/StreaksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StreaksViewController.swift
+//  Keep Up
+//
+//  Created by Lisa Tang on 12/19/20.
+//
+
+import UIKit
+
+class StreaksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
